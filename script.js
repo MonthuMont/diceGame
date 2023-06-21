0745f7936c9f6043008a36fdd626424c3d9ba4a9
MonthuMont <dadonov.stas2017@yandex.ru>
2023-06-22 00:38:35 +0400

    added: throw dice options

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use strict';
+// Elements selection
+const score0Points = document.getElementById('score--0');
+const score1Points = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+let throwDice = document.querySelector('.btn--roll');
+const current0Element = document.getElementById('current--0');
+// Game initial conditions;
+score0Points.textContent = 0;
+score1Points.textContent = 0;
+diceElement.classList.add('hidden');
+
+let currentScore0 = 0;
+let currentScore1 = 0;
+let activePlayer = 0;
+let totalScores = [0, 0];
+
+throwDice.addEventListener('click', function () {
+  let randomNum = Math.round(Math.random() * 5 + 1);
+  console.log(randomNum);
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice${randomNum}.png`;
+  if (randomNum !== 1) {
+    
+    currentScore0 += randomNum;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore0;
+    
+  }
+  else{
+    currentScore0 = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore0;
+    activePlayer = activePlayer === 0 ? 1: 0 ;
+    console.log('active player ' + activePlayer);
+    
+  }
+
+});
