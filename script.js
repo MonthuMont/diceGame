5d5287d49252a4abdd21129f9e20516067a34b9d
MonthuMont <dadonov.stas2017@yandex.ru>
2023-06-23 17:39:44 +0400

    added: throw dice options

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 let throwDice = document.querySelector('.btn--roll');
 const current0Element = document.getElementById('current--0');
+let player0 = document.querySelector('.player--active');
+let player1 = document.querySelector('.player--1');
 // Game initial conditions;
 score0Points.textContent = 0;
 score1Points.textContent = 0;
@@ -23,17 +25,21 @@ throwDice.addEventListener('click', function () {
   diceElement.classList.remove('hidden');
   diceElement.src = `dice${randomNum}.png`;
   if (randomNum !== 1) {
-    
     currentScore0 += randomNum;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore0;
-    
-  }
-  else{
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore0;
+  } else {
     currentScore0 = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore0;
-    activePlayer = activePlayer === 0 ? 1: 0 ;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     console.log('active player ' + activePlayer);
-    
+    if (activePlayer) {
+      player1.classList.add('player--active');
+      player0.classList.remove('player--active');
+    } else {
+      player0.classList.add('player--active');
+      player1.classList.remove('player--active');
+    }
   }
-
 });
