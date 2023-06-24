dd2fa08d382ab5d7e155d19fd6fe9364b22c9cf2
MonthuMont <dadonov.stas2017@yandex.ru>
2023-06-24 20:59:37 +0400

    added: save points and start new game options

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 'use strict';
 // Elements selection
-const score0Points = document.getElementById('score--0');
-const score1Points = document.getElementById('score--1');
+let score0Points = document.getElementById('score--0');
+let score1Points = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 let throwDice = document.querySelector('.btn--roll');
 const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
 let player0 = document.querySelector('.player--active');
 let player1 = document.querySelector('.player--1');
 // Game initial conditions;
 score0Points.textContent = 0;
 score1Points.textContent = 0;
 diceElement.classList.add('hidden');
+let switchActivePlayer = function () {
+  currentScore0 = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore0;
 
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  if (activePlayer) {
+    player1.classList.add('player--active');
+    player0.classList.remove('player--active');
+  } else {
+    player0.classList.add('player--active');
+    player1.classList.remove('player--active');
+  }
+};
 let currentScore0 = 0;
-let currentScore1 = 0;
 let activePlayer = 0;
 let totalScores = [0, 0];
 
@@ -29,17 +42,37 @@ throwDice.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore0;
   } else {
-    currentScore0 = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    console.log('active player ' + activePlayer);
-    if (activePlayer) {
-      player1.classList.add('player--active');
-      player0.classList.remove('player--active');
-    } else {
-      player0.classList.add('player--active');
-      player1.classList.remove('player--active');
-    }
+    switchActivePlayer();
   }
+});
+btnHold.addEventListener('click', function () {
+  totalScores[activePlayer] += currentScore0;
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    totalScores[activePlayer];
+  if (totalScores[activePlayer] >= 20) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    switchActivePlayer();
+  }
+});
+btnNew.addEventListener('click', function () {
+  currentScore0 = 0;
+  activePlayer = 0;
+  totalScores = [0, 0];
+  score0Points.textContent = totalScores[0];
+  score1Points.textContent = totalScores[1];
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  current0Element.textContent = currentScore0;
+  current1Element.textContent = currentScore0;
+ 
 });
